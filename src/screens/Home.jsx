5c2d77299f4f7d2f260052eaec013caba024e8ec
mukeshwagh9284 <mukeshwagh298@gmail.com>
2024-03-19 15:54:52 +0530

    changes

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Buttons from '../Components/Buttons'
+import React from "react";
+import Buttons from "../Components/Buttons";
 import { FaGoogle } from "react-icons/fa";
 
 const Home = () => {
- return (
+  return (
     <>
-    <Buttons logo={<FaGoogle />}>google</Buttons>
-  <div>
-   <button>Increase count</button>
-  </div>
-  </>
- )
-}
+      <Buttons logo={<FaGoogle />}>google</Buttons>
+      <div>
+        <button>Increase count</button>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
