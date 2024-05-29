ac95220d951768bc0131783658a7f2c5e9c09621
lalitj003 <lalitj003@gmail.com>
2024-05-29 07:36:17 +0530

    added new screens

--- src/navigation/routes.jsx
+++ src/navigation/routes.jsx
@@ -6,6 +6,9 @@ import SignIn from '../screens/SignIn'
 import Register from '../screens/Register'
 import Forgot_password from '../screens/Forgot_password'
 import EditProfile from '../screens/EditProfile'
+import Analytics from '../screens/Analytics'
+import {AccordionDetails} from '@mui/material'
+import Pricing from '../screens/Pricing'
 const NavigationRouter = () => {
  return (
   <Router>
@@ -16,6 +19,9 @@ const NavigationRouter = () => {
     <Route path='/register' element={<Register />} />
     <Route path='/forgotPassword' element={<Forgot_password />} />
     <Route path='/editProfile/:id' element={<EditProfile />} />
+    <Route path='/account' element={<AccordionDetails />} />
+    <Route path='/pricing' element={<Pricing />} />
+    <Route path='/analytics' element={<Analytics />} />
    </Routes>
   </Router>
  )
